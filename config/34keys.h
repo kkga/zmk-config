15f523f0a218fccb945b1ce887c035374d90a11b
Gadzhi Kharkharov <me@kkga.me>
2022-02-06 15:47:11 +0300

    update one more time

--- config/34keys.h
+++ config/34keys.h
@@ -1,4 +1,4 @@
-#define XXX &none
+#define xxx &none
 #define ___ &trans
 
 // Layer definitions
@@ -10,7 +10,7 @@
 
 // Custom keycodes
 #define MEH        LS(LA(LCTRL))
-#define TMUX       LC(G)
+#define TMUX       LC(A)
 #define D_LT       LC(LG(LEFT))
 #define D_RT       LC(LG(RIGHT))
 #define TAB_RT     LC(TAB)
